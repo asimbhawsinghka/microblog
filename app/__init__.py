dd95ea62a55fd00aea6b036e3b34dbddb0d745b4
asimbhawsinghka <asimbhawsinghka@gmail.com>
2022-08-12 16:46:14 +0530

    Gracefully handle errors

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -14,4 +13,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
