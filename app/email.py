22faa516ccabf8e6f2cb384cf095fe24f8dc5831
asimbhawsinghka <asimbhawsinghka@gmail.com>
2022-08-15 10:39:05 +0530

    Support asynchronous mails

--- app/email.py
+++ app/email.py
@@ -1,3 +1,5 @@
+from threading import Thread
+
 from flask import render_template
 from flask_mail import Message
 
@@ -5,11 +7,16 @@ from app import app
 from app import mail
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html_body = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
+
 
 
 def send_password_reset_email(user):
